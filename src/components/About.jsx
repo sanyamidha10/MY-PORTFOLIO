8f98eca112273f7649678a7b6136cae76543d14b
Sanya Midha <sanyamidha.10@gmail.com>
2023-05-13 20:12:21 +0530

    Hero section complete

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Tilt } from 'react-tilt'
+import {motion} from 'framer-motion';
+import {styles} from '../styles';
+import {services} from '../constants'
+import {fadeIn, textVariant} from '../utils/motion'
+
+const About = () => {
+  return (
+    <>
+      <motion.dev>
+        
+      </motion.dev>
+    </>
+  )
+}
+
+export default About
